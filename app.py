5bd30f11376addfe4f51262e877215db91354f5c
satinder <satindersingh472@gmail.com>
2023-01-29 11:24:48 -0700

    commented

---
+++ app.py
@@ -0,0 +1,23 @@
+walls = [(20, 9), (25, 9), (20, 9), (25, 9)]
+
+
+def calc_paint_amount(wall_measurements):
+
+  square_feet = 0
+
+  def calc_square_feet():
+    # declare square feet as a nonlocal variable so that it can become modifiable
+    nonlocal square_feet
+    for width, height in wall_measurements:
+      square_feet += width * height
+
+  def calc_gallons():
+    return square_feet / 400
+
+  calc_square_feet()
+
+  return calc_gallons()
+
+
+print('Number of paint gallons needed: ')
+print(str(calc_paint_amount(walls)))
